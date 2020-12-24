4fbd3140547a47c3034f689cf59a96e7776f3dda
Paweł Dąbrowski <fluorix@gmail.com>
2020-12-24 09:20:36 +0100

    created empty classes for implementation of interface

---
+++ src/main/java/com/facebook/genAlgo/evaluator/Variant1EvaluatorImpl.java
@@ -0,0 +1,11 @@
+package com.facebook.genAlgo.evaluator;
+
+import com.facebook.genAlgo.gene.Gene;
+
+public class Variant1EvaluatorImpl implements Evaluator {
+
+    @Override
+    public float setFitness(Gene gene) {
+        return 0;
+    }
+}
