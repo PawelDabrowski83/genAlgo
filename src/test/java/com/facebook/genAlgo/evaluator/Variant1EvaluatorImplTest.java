6a719374071f5c471947107fa64c41de7d4a2895
Paweł Dąbrowski <fluorix@gmail.com>
2020-12-24 10:08:35 +0100

    tests for close match in variant1

--- src/test/java/com/facebook/genAlgo/evaluator/Variant1EvaluatorImplTest.java
+++ src/test/java/com/facebook/genAlgo/evaluator/Variant1EvaluatorImplTest.java
@@ -13,7 +13,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /*
-evaluation formula 1 / (1+log(1+delta))
+evaluation formula 1 / (1+log10(1+delta))
  */
 public class Variant1EvaluatorImplTest {
 
@@ -22,8 +22,8 @@ public class Variant1EvaluatorImplTest {
 
     @DisplayName("Should setFitness calculate fitness for full match")
     @ParameterizedTest
-    @MethodSource("setFitnessArgumentsProvider")
-    void setFitness(float fitnessExpected, char target, char evaluatedValue) {
+    @MethodSource("setFitnessFullMatchArgumentsProvider")
+    void setFitnessFullMatch(float fitnessExpected, char target, char evaluatedValue) {
         // given
         evaluator = new Variant1EvaluatorImpl(target);
         when(gene.getValues()).thenReturn(new char[]{evaluatedValue});
@@ -35,13 +35,47 @@ public class Variant1EvaluatorImplTest {
         assertEquals(fitnessExpected, fitnessActual, 0.0001);
     }
 
-    private static Stream<Arguments> setFitnessArgumentsProvider() {
+    private static Stream<Arguments> setFitnessFullMatchArgumentsProvider() {
         return Stream.of(
                 Arguments.of(1, (char) 100, (char) 100),
                 Arguments.of(1, (char) 0, (char) 0),
                 Arguments.of(1, 'ß', 'ß'),
                 Arguments.of(1, ' ', ' '),
                 Arguments.of(1, Character.MAX_VALUE, Character.MAX_VALUE)
+        );
+    }
+
+    @DisplayName("Should setFitness calculate fitness for close match")
+    @ParameterizedTest
+    @MethodSource("setFitnessCloseMatchArgumentsProvider")
+    void setFitnessCloseMatch(float fitnessExpected, char target, char evaluatedValue) {
+        // given
+        evaluator = new Variant1EvaluatorImpl(target);
+        when(gene.getValues()).thenReturn(new char[]{evaluatedValue});
+
+        // when
+        float fitnessActual = evaluator.setFitness(gene);
+
+        // then
+        assertEquals(fitnessExpected, fitnessActual, 0.0001);
+    }
+
+    private static Stream<Arguments> setFitnessCloseMatchArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(0.7686218, (char) 100, (char) 99),
+                Arguments.of(0.6769925, (char) 100, (char) 98),
+                Arguments.of(0.7686218, (char) 100, (char) 101),
+                Arguments.of(0.7686218, (char) 0, (char) 1),
+                Arguments.of(0.6769925, (char) 0, (char) 2),
+                Arguments.of(0.62419635, (char) 0, (char) 3),
+                Arguments.of(0.7686218, 'ß', 'ß' + 1),
+                Arguments.of(0.7686218, 'ß', 'ß' - 1),
+                Arguments.of(0.6769925, 'ß', 'ß' + 2),
+                Arguments.of(0.6769925, 'ß', 'ß' - 2),
+                Arguments.of(0.7686218, ' ', ' ' + 1),
+                Arguments.of(0.7686218, ' ', ' ' - 1),
+                Arguments.of(0.7686218, Character.MAX_VALUE, Character.MAX_VALUE - 1),
+                Arguments.of(0.6769925, Character.MAX_VALUE, Character.MAX_VALUE - 2)
         );
     }
 }
