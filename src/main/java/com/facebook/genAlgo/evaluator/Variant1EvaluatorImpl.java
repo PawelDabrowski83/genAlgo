16ba31d130728bd396c0b33836bac8602819becf
Surfboy678 <janusz.brodacki@gmail.com>
2020-12-30 21:44:10 +0100

    added tests to variants Emulator

--- src/main/java/com/facebook/genAlgo/evaluator/Variant1EvaluatorImpl.java
+++ src/main/java/com/facebook/genAlgo/evaluator/Variant1EvaluatorImpl.java
@@ -6,15 +6,15 @@ import static java.lang.Math.log;
 
 public class Variant1EvaluatorImpl implements Evaluator {
 
-    private final char target;
+  private final char target;
 
-    public Variant1EvaluatorImpl(char target) {
-        this.target = target;
-    }
+  public Variant1EvaluatorImpl(char target) {
+    this.target = target;
+  }
 
-    @Override
-    public float setFitness(Gene gene) {
-        float delta = gene.getValues()[0] - target;
-        return (float) (1 / (1+log(1 + delta)));
-    }
+  @Override
+  public float setFitness(Gene gene) {
+    float delta = gene.getValues()[0] - target;
+    return (float) (1 / (1 + log(1 + delta)));
+  }
 }
