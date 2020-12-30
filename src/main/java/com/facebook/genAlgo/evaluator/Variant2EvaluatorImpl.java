639c67bb9cc21fc6bb22f3b0401fed4394e78edc
Paweł Dąbrowski <fluorix@gmail.com>
2020-12-30 22:21:32 +0100

    bugfix - test for Variant2 tested Variant1 - copy&paste error

--- src/main/java/com/facebook/genAlgo/evaluator/Variant2EvaluatorImpl.java
+++ src/main/java/com/facebook/genAlgo/evaluator/Variant2EvaluatorImpl.java
@@ -4,6 +4,12 @@ import com.facebook.genAlgo.gene.Gene;
 
 public class Variant2EvaluatorImpl implements Evaluator {
 
+    private final char target;
+
+    public Variant2EvaluatorImpl(char target) {
+        this.target = target;
+    }
+
     @Override
     public float setFitness(Gene gene) {
         return 0;
