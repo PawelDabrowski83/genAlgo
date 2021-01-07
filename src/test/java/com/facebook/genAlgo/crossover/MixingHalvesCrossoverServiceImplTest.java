d03755a06cdd2b06169ea52de4030603e2231d14
Kamil <s.kamilx@gmail.com>
2021-01-07 23:53:01 +0100

    Test method added

--- src/test/java/com/facebook/genAlgo/crossover/MixingHalvesCrossoverServiceImplTest.java
+++ src/test/java/com/facebook/genAlgo/crossover/MixingHalvesCrossoverServiceImplTest.java
@@ -1,12 +1,29 @@
 package com.facebook.genAlgo.crossover;
 
+import com.facebook.genAlgo.gene.Gene;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.when;
 
 public class MixingHalvesCrossoverServiceImplTest {
 
+    @Mock
+    Gene gene1, gene2;
 
-    @Test
-    void shouldMixingHalvesOfGene() {
+    CrossoverService crossoverService;
+
+    @ParameterizedTest
+    void shouldMixingHalvesOfGene(char gene1Val, char gene2Val, char) {
+
+        crossoverService = new MixingHalvesCrossoverImpl();
+
+        when(gene1.getValue()).thenReturn('a');
+        when(gene2.getValue()).thenReturn('b');
+
+        crossoverService.cross(gene1, gene2);
+
 
     }
 }
