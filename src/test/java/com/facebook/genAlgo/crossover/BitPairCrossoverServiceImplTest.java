06ce31b4b42823b69319a733048fb773d93b3b2a
Kamil <s.kamilx@gmail.com>
2021-01-09 00:24:44 +0100

    Added Argument Stream method in BitPairCrossoverServiceImplTest

--- src/test/java/com/facebook/genAlgo/crossover/BitPairCrossoverServiceImplTest.java
+++ src/test/java/com/facebook/genAlgo/crossover/BitPairCrossoverServiceImplTest.java
@@ -4,8 +4,11 @@ import com.facebook.genAlgo.gene.Gene;
 import com.facebook.genAlgo.utils.RandomProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
+
+import java.util.stream.Stream;
 
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.when;
@@ -16,7 +19,7 @@ public class BitPairCrossoverServiceImplTest {
     RandomProvider randomProviderGene1, randomProviderGene2;
 
     @ParameterizedTest
-    @MethodSource("shouldMixEvenBitesProvider")
+    @MethodSource("shouldProvideBitPairProvider")
     void shouldProvideBitPairCrossover(int gene1Val, int gene2Val, char gene1ValueAfterCross, char gene2ValueAfterCross) {
         // given
         CrossoverService crossoverService = new OddBitesCrossoverImpl();
@@ -33,4 +36,10 @@ public class BitPairCrossoverServiceImplTest {
         Assertions.assertEquals(gene2.getValue(), gene2ValueAfterCross);
     }
 
+    private Stream<Arguments> shouldProvideBitPairProvider() {
+        return Stream.of(
+                Arguments.of(33, 5000, (char) 169, (char) 4864),
+                Arguments.of(70, 65535, (char) 52430, (char) 13175)
+        );
+    }
 }
