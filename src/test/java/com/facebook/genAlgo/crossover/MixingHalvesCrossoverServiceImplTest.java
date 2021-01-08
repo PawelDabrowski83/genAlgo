df0d1450828fd21c12a090f555e71e014beaf7d1
Kamil <s.kamilx@gmail.com>
2021-01-08 21:19:40 +0100

    Stream with Arguments to test added

--- src/test/java/com/facebook/genAlgo/crossover/MixingHalvesCrossoverServiceImplTest.java
+++ src/test/java/com/facebook/genAlgo/crossover/MixingHalvesCrossoverServiceImplTest.java
@@ -1,9 +1,15 @@
 package com.facebook.genAlgo.crossover;
 
 import com.facebook.genAlgo.gene.Gene;
+import com.sun.jdi.connect.Connector;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
+
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.when;
 
@@ -15,7 +21,8 @@ public class MixingHalvesCrossoverServiceImplTest {
     CrossoverService crossoverService;
 
     @ParameterizedTest
-    void shouldMixingHalvesOfGene(char gene1Val, char gene2Val, char) {
+    @MethodSource("shouldMixingHalvesOfGeneProvider")
+    void shouldMixingHalvesOfGene(char gene1Val, char gene2Val, char gene1ValueAfterCross, char gene2ValueAfterCross) {
 
         crossoverService = new MixingHalvesCrossoverImpl();
 
@@ -24,6 +31,14 @@ public class MixingHalvesCrossoverServiceImplTest {
 
         crossoverService.cross(gene1, gene2);
 
+        Assertions.assertEquals();
+    }
 
+    private Stream<Arguments> shouldMixingHalvesOfGeneProvider() {
+        return Stream.of(
+                Arguments.of('ᆙ', '$', (char) 4388, (char) 153),
+                Arguments.of('!', 'ᎈ', (char) 136, (char) 4897),
+                Arguments.of('F', '\uFFFF', (char) 255, (char) 65350)
+        );
     }
 }
