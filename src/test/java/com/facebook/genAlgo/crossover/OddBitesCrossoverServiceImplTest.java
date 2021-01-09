ff2513ef34325a96a064b0748f7d110d49b45285
Kamil <s.kamilx@gmail.com>
2021-01-09 19:00:57 +0100

    Delete unused imports in  OddBitesCrossoverServiceImplTest

--- src/test/java/com/facebook/genAlgo/crossover/OddBitesCrossoverServiceImplTest.java
+++ src/test/java/com/facebook/genAlgo/crossover/OddBitesCrossoverServiceImplTest.java
@@ -7,12 +7,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Mock;
 
 import java.util.stream.Stream;
-
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.when;
 
 public class OddBitesCrossoverServiceImplTest {
 
