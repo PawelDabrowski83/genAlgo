92b2b13127957dd97d2ac2f708d9915325437339
Kamil <s.kamilx@gmail.com>
2021-01-09 19:05:48 +0100

    Refactor EvenBitesCrossoverServiceImplTest

--- src/test/java/com/facebook/genAlgo/crossover/EvenBitesCrossoverServiceImplTest.java
+++ src/test/java/com/facebook/genAlgo/crossover/EvenBitesCrossoverServiceImplTest.java
@@ -2,38 +2,34 @@ package com.facebook.genAlgo.crossover;
 
 import com.facebook.genAlgo.gene.Gene;
 import com.facebook.genAlgo.utils.RandomProvider;
+import com.facebook.genAlgo.utils.RandomProviderImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Mock;
 
 import java.util.stream.Stream;
 
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.when;
 
 public class EvenBitesCrossoverServiceImplTest {
 
-    @Mock
-    RandomProvider randomProviderGene1, randomProviderGene2;
-
     @ParameterizedTest
     @MethodSource("evenBitesCrossoverProvider")
-    void shouldProvideEvenBitesCrossover(int gene1Val, int gene2Val, char gene1ValueAfterCross, char gene2ValueAfterCross) {
+    void shouldProvideEvenBitesCrossover(int gene1Val, int gene2Val, char gene1ValueExpected, char gene2ValueExpected) {
         // given
+        RandomProvider randomProvider = new RandomProviderImpl();
         CrossoverService evenBitesService = new EvenBitesCrossoverServiceImpl();
-        when(randomProviderGene1.getRandom(anyInt())).thenReturn(gene1Val);
-        when(randomProviderGene2.getRandom(anyInt())).thenReturn(gene2Val);
+        Gene gene1 = new Gene(randomProvider);
+        Gene gene2 = new Gene(randomProvider);
+        gene1.setValue(gene1Val);
+        gene2.setValue(gene2Val);
 
         // when
-        Gene gene1 = new Gene(randomProviderGene1);
-        Gene gene2 = new Gene(randomProviderGene2);
         evenBitesService.cross(gene1, gene2);
 
         // then
-        Assertions.assertEquals(gene1.getValue(), gene1ValueAfterCross);
-        Assertions.assertEquals(gene2.getValue(), gene2ValueAfterCross);
+        Assertions.assertEquals(gene1.getValue(), gene1ValueExpected);
+        Assertions.assertEquals(gene2.getValue(), gene2ValueExpected);
     }
 
 
