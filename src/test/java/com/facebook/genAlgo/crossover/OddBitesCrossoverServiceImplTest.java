c3a8fed82ce9e3971eecf0b393cf7196fb2f2912
Kamil <s.kamilx@gmail.com>
2021-01-09 19:00:02 +0100

    Refactor OddBitesCrossoverServiceImplTest

--- src/test/java/com/facebook/genAlgo/crossover/OddBitesCrossoverServiceImplTest.java
+++ src/test/java/com/facebook/genAlgo/crossover/OddBitesCrossoverServiceImplTest.java
@@ -2,6 +2,7 @@ package com.facebook.genAlgo.crossover;
 
 import com.facebook.genAlgo.gene.Gene;
 import com.facebook.genAlgo.utils.RandomProvider;
+import com.facebook.genAlgo.utils.RandomProviderImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -15,13 +16,11 @@ import static org.mockito.Mockito.when;
 
 public class OddBitesCrossoverServiceImplTest {
 
-
-    RandomProvider randomProvider;
-
     @ParameterizedTest
     @MethodSource("oddBitesCrossoverProvider")
     void shouldProvideOddBitesCrossover(int gene1Val, int gene2Val, char gene1ValueExpected, char gene2ValueExpected) {
         // given
+        RandomProvider randomProvider = new RandomProviderImpl();
         CrossoverService oddBitesService = new OddBitesCrossoverServiceImpl();
         Gene gene1 = new Gene(randomProvider);
         Gene gene2 = new Gene(randomProvider);
@@ -32,8 +31,8 @@ public class OddBitesCrossoverServiceImplTest {
         oddBitesService.cross(gene1, gene2);
 
         // then
-        Assertions.assertEquals(gene1.getValue(), gene1ValueAfterCross);
-        Assertions.assertEquals(gene2.getValue(), gene2ValueAfterCross);
+        Assertions.assertEquals(gene1.getValue(), gene1ValueExpected);
+        Assertions.assertEquals(gene2.getValue(), gene2ValueExpected);
     }
 
 
