837059d25d553654e73598cd70b87837552472e7
Kamil <s.kamilx@gmail.com>
2021-01-09 19:19:16 +0100

    Create empty class BitPairCrossoverServiceImpl

---
+++ src/main/java/com/facebook/genAlgo/crossover/BitPairCrossoverServiceImpl.java
@@ -0,0 +1,12 @@
+package com.facebook.genAlgo.crossover;
+
+import com.facebook.genAlgo.gene.Gene;
+
+public class BitPairCrossoverServiceImpl implements CrossoverService {
+
+
+    @Override
+    public void cross(Gene gene1, Gene gene2) {
+
+    }
+}
