7015727fee12548ff26a92f43f9919f8a0c2e1cb
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-10 11:52:48 +0100

    Refactored Exception outputs to final static strings.

--- src/main/java/com/facebook/genAlgo/utils/BitwiseUtils.java
+++ src/main/java/com/facebook/genAlgo/utils/BitwiseUtils.java
@@ -3,20 +3,22 @@ package com.facebook.genAlgo.utils;
 public class BitwiseUtils {
 
     public static final int BIT_MASK = 0b1111_1111_1111_1111_1111_1111_1111_1111;
+    public static final String INDEX_BELOW_ZERO_EXCEPTION = "Index cannot be below zero.";
+    public static final String INCORRECT_BIT_VALUE_EXCEPTION = "Bit value can be only 0 or 1.";
 
     public int getBit(int number, int index) {
         if (index < 0) {
-            throw new IllegalArgumentException("Index cannot be below zero.");
+            throw new IllegalArgumentException(INDEX_BELOW_ZERO_EXCEPTION);
         }
         return (number >> index) & 1;
     }
 
     public int setBit(int number, int index, int value) {
         if (index < 0) {
-            throw new IllegalArgumentException("Index cannot be below zero.");
+            throw new IllegalArgumentException(INDEX_BELOW_ZERO_EXCEPTION);
         }
         if (value != 0 && value != 1) {
-            throw new IllegalArgumentException("Bit value can be only 0 or 1.");
+            throw new IllegalArgumentException(INCORRECT_BIT_VALUE_EXCEPTION);
         }
         int currentBitValue = getBit(number, index);
         if (currentBitValue != value) {
