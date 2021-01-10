b659d40881074fb0e42bb947c43ba1b476de4fbd
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-10 13:04:19 +0100

    Added new dependency to MixingHalvesCrossoverServiceImpl - I want to use an instance of BitwiseUtils.

--- src/main/java/com/facebook/genAlgo/crossover/MixingHalvesCrossoverServiceImpl.java
+++ src/main/java/com/facebook/genAlgo/crossover/MixingHalvesCrossoverServiceImpl.java
@@ -1,8 +1,15 @@
 package com.facebook.genAlgo.crossover;
 
 import com.facebook.genAlgo.gene.Gene;
+import com.facebook.genAlgo.utils.BitwiseUtils;
 
 public class MixingHalvesCrossoverServiceImpl implements CrossoverService{
+
+    private final BitwiseUtils bitwiseUtils;
+
+    public MixingHalvesCrossoverServiceImpl() {
+        this.bitwiseUtils = new BitwiseUtils();
+    }
 
     @Override
     public void cross(Gene gene1, Gene gene2) {
