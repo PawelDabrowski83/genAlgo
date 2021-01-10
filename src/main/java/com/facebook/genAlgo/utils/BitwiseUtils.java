5fdde2a5e53f7d44abc91da56ea99c563a0c4a5a
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-10 11:07:44 +0100

    Implemented bitIndex checking in getBit()

--- src/main/java/com/facebook/genAlgo/utils/BitwiseUtils.java
+++ src/main/java/com/facebook/genAlgo/utils/BitwiseUtils.java
@@ -3,6 +3,9 @@ package com.facebook.genAlgo.utils;
 public class BitwiseUtils {
 
     public int getBit(int number, int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Index cannot be below zero.");
+        }
         return (number >> index) & 1;
     }
 }
