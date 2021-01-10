4e4dfa268d04f12cd6a38e2409d972907fc56255
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-10 13:32:33 +0100

    Started working on MixingHalvesCrossoverServiceImpl but after realising Gene.setValue is not ready I need to pause.

--- src/main/java/com/facebook/genAlgo/crossover/MixingHalvesCrossoverServiceImpl.java
+++ src/main/java/com/facebook/genAlgo/crossover/MixingHalvesCrossoverServiceImpl.java
@@ -13,6 +13,16 @@ public class MixingHalvesCrossoverServiceImpl implements CrossoverService{
 
     @Override
     public void cross(Gene gene1, Gene gene2) {
+        int targetGene1 = gene1.getValue();
+        int targetGene2 = gene2.getValue();
 
+        int secondHalfGene1 = bitwiseUtils.getByte(targetGene2, 1);
+        int secondHalfGene2 = bitwiseUtils.getByte(targetGene1, 1);
+
+        targetGene1 = bitwiseUtils.setByte(targetGene1, 1, secondHalfGene1);
+        targetGene2 = bitwiseUtils.setByte(targetGene2, 1, secondHalfGene2);
+
+        gene1.setValue((char) targetGene1);
+        gene2.setValue((char) targetGene2);
     }
 }
