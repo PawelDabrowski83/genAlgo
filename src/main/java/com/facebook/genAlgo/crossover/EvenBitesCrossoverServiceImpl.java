702a700c7edf5f3dae30754e8aebeb5eb9397a28
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-11 09:55:05 +0100

    Added BitwiseUtils to EvenBitesCrossoverService.

--- src/main/java/com/facebook/genAlgo/crossover/EvenBitesCrossoverServiceImpl.java
+++ src/main/java/com/facebook/genAlgo/crossover/EvenBitesCrossoverServiceImpl.java
@@ -1,8 +1,15 @@
 package com.facebook.genAlgo.crossover;
 
 import com.facebook.genAlgo.gene.Gene;
+import com.facebook.genAlgo.utils.BitwiseUtils;
 
 public class EvenBitesCrossoverServiceImpl implements CrossoverService {
+
+    private final BitwiseUtils bitwiseUtils;
+
+    public EvenBitesCrossoverServiceImpl() {
+        bitwiseUtils = new BitwiseUtils();
+    }
 
     @Override
     public void cross(Gene gene1, Gene gene2) {
