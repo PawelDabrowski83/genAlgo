8bb04ebd2787358192961ed39083078efd2c748d
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-11 09:57:38 +0100

    Added BitwiseUtils to OddBitsCrossoverService.

--- src/main/java/com/facebook/genAlgo/crossover/OddBitesCrossoverServiceImpl.java
+++ src/main/java/com/facebook/genAlgo/crossover/OddBitesCrossoverServiceImpl.java
@@ -1,8 +1,15 @@
 package com.facebook.genAlgo.crossover;
 
 import com.facebook.genAlgo.gene.Gene;
+import com.facebook.genAlgo.utils.BitwiseUtils;
 
 public class OddBitesCrossoverServiceImpl implements CrossoverService {
+
+    private final BitwiseUtils bitwiseUtils;
+
+    public OddBitesCrossoverServiceImpl() {
+        this.bitwiseUtils = new BitwiseUtils();
+    }
 
     @Override
     public void cross(Gene gene1, Gene gene2) {
