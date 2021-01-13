49e2575db38d310bd354e30fb5d11883e58c9473
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-13 22:15:02 +0100

    Introduce RandomProvider dependency as a source of random numbers for mutation calculations.

--- src/main/java/com/facebook/genAlgo/mutator/SingleMutator.java
+++ src/main/java/com/facebook/genAlgo/mutator/SingleMutator.java
@@ -1,8 +1,15 @@
 package com.facebook.genAlgo.mutator;
 
 import com.facebook.genAlgo.gene.Gene;
+import com.facebook.genAlgo.utils.RandomProvider;
 
 public class SingleMutator implements MutatorService {
+
+    private final RandomProvider randomProvider;
+
+    public SingleMutator(RandomProvider randomProvider) {
+        this.randomProvider = randomProvider;
+    }
 
     @Override
     public void mutate(Gene gene) {
