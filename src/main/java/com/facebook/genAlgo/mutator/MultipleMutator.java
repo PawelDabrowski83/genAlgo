44c806f04f4b1f75ef42bcd7acc791e12ceef1b9
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-13 22:16:02 +0100

    Add MultipleMutator with empty implementation of mutate().

---
+++ src/main/java/com/facebook/genAlgo/mutator/MultipleMutator.java
@@ -0,0 +1,11 @@
+package com.facebook.genAlgo.mutator;
+
+import com.facebook.genAlgo.gene.Gene;
+
+public class MultipleMutator implements MutatorService {
+
+    @Override
+    public void mutate(Gene gene) {
+
+    }
+}
