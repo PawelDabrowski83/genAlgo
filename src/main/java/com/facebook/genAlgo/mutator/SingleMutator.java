edb9259b3a86847af2fff69e81407e42431e5c7b
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-13 22:13:14 +0100

    Create SingleMutator with empty implementation of mutate().

---
+++ src/main/java/com/facebook/genAlgo/mutator/SingleMutator.java
@@ -0,0 +1,11 @@
+package com.facebook.genAlgo.mutator;
+
+import com.facebook.genAlgo.gene.Gene;
+
+public class SingleMutator implements MutatorService {
+
+    @Override
+    public void mutate(Gene gene) {
+
+    }
+}
