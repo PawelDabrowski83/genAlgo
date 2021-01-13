bd0163c00be758c4e39a6524423ae78788d7253e
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-13 22:16:42 +0100

    Add RandomProvider dependency.

--- src/main/java/com/facebook/genAlgo/mutator/MultipleMutator.java
+++ src/main/java/com/facebook/genAlgo/mutator/MultipleMutator.java
@@ -1,8 +1,15 @@
 package com.facebook.genAlgo.mutator;
 
 import com.facebook.genAlgo.gene.Gene;
+import com.facebook.genAlgo.utils.RandomProvider;
 
 public class MultipleMutator implements MutatorService {
+
+    private final RandomProvider randomProvider;
+
+    public MultipleMutator(RandomProvider randomProvider) {
+        this.randomProvider = randomProvider;
+    }
 
     @Override
     public void mutate(Gene gene) {
