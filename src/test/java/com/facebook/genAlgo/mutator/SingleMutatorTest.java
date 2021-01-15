199c8eb737aa55e303eb85145cd546506d1b0c7a
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-15 08:37:36 +0100

    Test SingleMutator whether mutation with mutationChange 1 (100%) changes gene value.

--- src/test/java/com/facebook/genAlgo/mutator/SingleMutatorTest.java
+++ src/test/java/com/facebook/genAlgo/mutator/SingleMutatorTest.java
@@ -4,14 +4,46 @@ import com.facebook.genAlgo.gene.Gene;
 import com.facebook.genAlgo.utils.RandomProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class SingleMutatorTest {
 
     RandomProvider randomProvider = mock(RandomProvider.class);
-    MutatorService mutatorService = new SingleMutator(randomProvider);
+    MutatorService mutatorService;
+
+    @DisplayName("Should mutate change Gene.value when mutation should be guaranteed.")
+    @ParameterizedTest
+    @MethodSource("mutateGuaranteedArgumentsProvider")
+    void mutateGuaranteed(int geneValue) {
+        // given
+        mutatorService = new SingleMutator(randomProvider, 1);
+        Gene gene = new Gene(randomProvider);
+        gene.setValue((char) geneValue);
+        int initialGeneValue = gene.getValue();
+
+        // when
+        mutatorService.mutate(gene);
+        int actualGeneValue = gene.getValue();
+
+        // then
+        assertNotEquals(initialGeneValue, actualGeneValue);
+    }
+
+    private static Stream<Arguments> mutateGuaranteedArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(0),
+                Arguments.of(100),
+                Arguments.of(10000),
+                Arguments.of(Character.MAX_VALUE)
+        );
+    }
 
 }
