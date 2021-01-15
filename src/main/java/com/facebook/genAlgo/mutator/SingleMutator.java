d9fe31ccf396e29ddc6dcf0d8357083bb5f68014
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-15 08:13:00 +0100

    Add mutationChance as a field of Mutator impl.

--- src/main/java/com/facebook/genAlgo/mutator/SingleMutator.java
+++ src/main/java/com/facebook/genAlgo/mutator/SingleMutator.java
@@ -6,9 +6,11 @@ import com.facebook.genAlgo.utils.RandomProvider;
 public class SingleMutator implements MutatorService {
 
     private final RandomProvider randomProvider;
+    private float mutationChance;
 
-    public SingleMutator(RandomProvider randomProvider) {
+    public SingleMutator(RandomProvider randomProvider, float mutationChance) {
         this.randomProvider = randomProvider;
+        this.mutationChance = mutationChance;
     }
 
     @Override
--- src/test/java/com/facebook/genAlgo/mutator/SingleMutatorTest.java
+++ src/test/java/com/facebook/genAlgo/mutator/SingleMutatorTest.java
@@ -1,8 +1,13 @@
 package com.facebook.genAlgo.mutator;
 
+import com.facebook.genAlgo.gene.Gene;
 import com.facebook.genAlgo.utils.RandomProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SingleMutatorTest {
 
