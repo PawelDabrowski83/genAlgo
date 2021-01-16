5ea5968bd944374570139b7e26832ed401d78a78
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-16 20:11:00 +0100

    Add test mutate() to check if mutation occurs. Add Mockito.doAnswer to prepare sequence of values returned by randomProvider mock, while randomizing bit positions to mutate.

--- src/test/java/com/facebook/genAlgo/mutator/MultipleMutatorTest.java
+++ src/test/java/com/facebook/genAlgo/mutator/MultipleMutatorTest.java
@@ -6,11 +6,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.*;
 
 public class MultipleMutatorTest {
 
@@ -41,6 +46,49 @@ public class MultipleMutatorTest {
                 Arguments.of(100),
                 Arguments.of(10000),
                 Arguments.of(Character.MAX_VALUE)
+        );
+    }
+
+    @DisplayName("Should mutate work properly when change occurs")
+    @ParameterizedTest
+    @MethodSource("mutateArgumentsProvider")
+    void mutate(char geneValue, char expectedGeneValue, int mutationTimes, int mutationStep, float mutationChance, float mutationScore) {
+        // given
+        Gene gene = new Gene(randomProvider);
+        gene.setValue(geneValue);
+        MutatorService mutator = new MultipleMutator(randomProvider, mutationChance);
+        doAnswer(new Answer<Integer>() {
+            private int count = 0;
+            public Integer answer(InvocationOnMock invocation) {
+                count++;
+                if (count == 1) {
+                    return mutationTimes;
+                }
+                return mutationStep + count - 1;
+            }
+        }).when(randomProvider).getInt(anyInt());
+        when(randomProvider.getFloat()).thenReturn(mutationScore);
+
+        // when
+        mutator.mutate(gene);
+        char actualGeneValue = gene.getValue();
+
+        // then
+        assertEquals(expectedGeneValue, actualGeneValue);
+    }
+
+    private static Stream<Arguments> mutateArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(Character.MIN_VALUE, (char) 1, 1, 0, 1, 0.5f),
+                Arguments.of((char) 0b1100_0111, (char) 0b1100_0001, 2, 1, 0.25f, 0.24f),
+                Arguments.of((char) 0b1100_0111, (char) 0b1111_1111, 3, 3, 0.25f, 0.24f),
+                Arguments.of((char) 0b1100_0111, (char) 0b1111_1100_0111, 4, 8, 0.25f, 0.24f),
+                Arguments.of((char) 0b0011_1101_0011_0001, (char) 0b0011_1101_0010_1110, 5, 0, 0.76f, 0.75f),
+                Arguments.of((char) 0b0011_1101_0011_0001, (char) 0b0011_1101_1100_1101, 6, 2, 0.76f, 0.75f),
+                Arguments.of((char) 0b0011_1101_0011_0001, (char) 0b0011_1010_1100_0001, 7, 4, 0.76f, 0.75f),
+                Arguments.of((char) 0b0011_1101_0011_0001, (char) 0b1100_0001_0011_0001, 7, 9, 0.76f, 0.75f),
+                Arguments.of((char) 0b0011_1101_0011_0001, (char) 0b0010_1101_0011_0001, 1, 12, 0.76f, 0.75f),
+                Arguments.of((char) 0b0011_1101_0011_0001, (char) 0b1011_1010_1100_1111, 10, 1, 0.76f, 0.75f)
         );
     }
 }
