ca22851ecc57483441556f4d3f92dfc66019d417
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-16 18:56:57 +0100

    Add field float mutationChance to MultipleMutator with getter, setter and constructor.

--- src/main/java/com/facebook/genAlgo/mutator/MultipleMutator.java
+++ src/main/java/com/facebook/genAlgo/mutator/MultipleMutator.java
@@ -6,9 +6,19 @@ import com.facebook.genAlgo.utils.RandomProvider;
 public class MultipleMutator implements MutatorService {
 
     private final RandomProvider randomProvider;
+    private float mutationChance;
 
-    public MultipleMutator(RandomProvider randomProvider) {
+    public MultipleMutator(RandomProvider randomProvider, float mutationChance) {
         this.randomProvider = randomProvider;
+        this.mutationChance = mutationChance;
+    }
+
+    public float getMutationChance() {
+        return mutationChance;
+    }
+
+    public void setMutationChance(float mutationChance) {
+        this.mutationChance = mutationChance;
     }
 
     @Override
