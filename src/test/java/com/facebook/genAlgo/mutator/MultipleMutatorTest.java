a57119a221a99e9c9bffd9eaa1ddf419b3a3556e
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-16 18:57:56 +0100

    Set up MultipleMutatorTest.

---
+++ src/test/java/com/facebook/genAlgo/mutator/MultipleMutatorTest.java
@@ -0,0 +1,11 @@
+package com.facebook.genAlgo.mutator;
+
+import com.facebook.genAlgo.utils.RandomProvider;
+
+import static org.mockito.Mockito.mock;
+
+public class MultipleMutatorTest {
+
+    RandomProvider randomProvider = mock(RandomProvider.class);
+    MutatorService mutatorService;
+}
