1055f07d4a7ba82e0650b2d8064042559f2569f9
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-18 23:02:06 +0100

    Remove unused import.

--- src/test/java/com/facebook/genAlgo/mutator/MultipleMutatorTest.java
+++ src/test/java/com/facebook/genAlgo/mutator/MultipleMutatorTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
