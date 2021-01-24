503ad6de9f909cecfcae2cc1d0c62191757cb758
Kamil <s.kamilx@gmail.com>
2021-01-24 01:19:38 +0100

    Add GenePoolTest

---
+++ src/test/java/com/facebook/genAlgo/genepool/GenePoolTest.java
@@ -0,0 +1,15 @@
+package com.facebook.genAlgo.genepool;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenePoolTest {
+
+
+    public void shouldInitializedGenes() {
+        // given
+        GenePool genePool = new GenePool()
+
+
+
+    }
+}
