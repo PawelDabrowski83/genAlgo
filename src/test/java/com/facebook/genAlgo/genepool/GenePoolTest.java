b46358f67226c2906739c4ad9ed22f18de4ca199
Kamil <s.kamilx@gmail.com>
2021-02-14 00:18:39 +0100

    Refactor remove unused static imports in GenePoolTest

--- src/test/java/com/facebook/genAlgo/genepool/GenePoolTest.java
+++ src/test/java/com/facebook/genAlgo/genepool/GenePoolTest.java
@@ -8,7 +8,6 @@ import com.facebook.genAlgo.mutator.MutatorService;
 import com.facebook.genAlgo.utils.RandomProvider;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
