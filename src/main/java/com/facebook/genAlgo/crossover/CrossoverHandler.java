02e65cc22dbd6feb88024dd786ee829741293d11
Kamil <s.kamilx@gmail.com>
2021-02-14 00:10:43 +0100

    Add fitnessSort method to CrossoverHandler

--- src/main/java/com/facebook/genAlgo/crossover/CrossoverHandler.java
+++ src/main/java/com/facebook/genAlgo/crossover/CrossoverHandler.java
@@ -1,4 +1,27 @@
 package com.facebook.genAlgo.crossover;
 
+import com.facebook.genAlgo.gene.Gene;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class CrossoverHandler {
+
+    private final CrossoverService crossoverService;
+
+    public CrossoverHandler(CrossoverService crossoverService) {
+        this.crossoverService = crossoverService;
+    }
+
+    public void performCross(List<Gene> geneList) {
+        fitnessSort(geneList);
+        for (int i = 0; i < geneList.size(); i = i = i + 2) {
+            crossoverService.cross(geneList.get(i), geneList.get(i + 1));
+        }
+    }
+
+
+    private void fitnessSort(List<Gene> geneList) {
+        geneList.sort(Comparator.comparing((x) -> x.getFitness()));
+    }
 }
