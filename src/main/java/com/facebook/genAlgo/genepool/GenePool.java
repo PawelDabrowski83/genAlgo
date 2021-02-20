26cceeb20fe9ce4549ed99a60043dcb8fee18a9a
Kamil <s.kamilx@gmail.com>
2021-02-20 16:47:38 +0100

    Change constructor in GenePool

--- src/main/java/com/facebook/genAlgo/genepool/GenePool.java
+++ src/main/java/com/facebook/genAlgo/genepool/GenePool.java
@@ -12,29 +12,15 @@ import java.util.List;
 
 public class GenePool {
 
-    private final RandomProvider randomProvider;
-    private final MutatorService mutatorService;
-    private final Evaluator evaluator;
-    private final CrossoverHandler crossoverHandler;
-    private final List<Gene> poolOfGenes;
+    private GenePoolService genePoolService;
     private int generation;
 
-    public GenePool(RandomProvider randomProvider, MutatorService mutatorService, Evaluator evaluator,
-                    CrossoverHandler crossoverHandler, int size) {
-        this.randomProvider = randomProvider;
-        this.mutatorService = mutatorService;
-        this.evaluator = evaluator;
-        this.crossoverHandler = crossoverHandler;
-        this.poolOfGenes = initializeGenes(size);
+    public GenePool(GenePoolService genePoolService) {
+        this.genePoolService = genePoolService;
     }
-
 
     public void performEvolution() {
         generation++;
-    }
-
-    public List<Gene> getPoolOfGenes() {
-        return poolOfGenes;
     }
 
     public int getGeneration() {
