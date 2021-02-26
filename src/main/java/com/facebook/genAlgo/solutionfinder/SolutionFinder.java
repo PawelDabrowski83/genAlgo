24336855113258aae1287b0040a0c150a1607e74
Kamil <s.kamilx@gmail.com>
2021-02-26 23:41:38 +0100

    Add target field and constructor to SolutionFinder

--- src/main/java/com/facebook/genAlgo/solutionfinder/SolutionFinder.java
+++ src/main/java/com/facebook/genAlgo/solutionfinder/SolutionFinder.java
@@ -1,4 +1,17 @@
 package com.facebook.genAlgo.solutionfinder;
 
+import com.facebook.genAlgo.gene.Gene;
+
+import java.util.List;
+
 public class SolutionFinder {
+    
+    private final char target;
+
+    public SolutionFinder(char target) {
+        this.target = target;
+    }
+
+    public Boolean findSolution(List<Gene> geneList) {
+    }
 }
