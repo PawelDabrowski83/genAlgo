7d48a0739346d9ef29962b2a05f447994e69bb85
Paweł Dąbrowski <fluorix@gmail.com>
2021-02-28 21:33:33 +0100

    Create dependencies for new GenePoolService.

--- src/test/java/com/facebook/genAlgo/MainIntegrationTest.java
+++ src/test/java/com/facebook/genAlgo/MainIntegrationTest.java
@@ -1,6 +1,31 @@
 package com.facebook.genAlgo;
 
+import com.facebook.genAlgo.crossover.BitPairCrossoverServiceImpl;
+import com.facebook.genAlgo.crossover.CrossoverHandler;
+import com.facebook.genAlgo.crossover.CrossoverService;
+import com.facebook.genAlgo.evaluator.Evaluator;
+import com.facebook.genAlgo.evaluator.LogarithmicEvaluatorImpl;
+import com.facebook.genAlgo.genepool.GenePoolService;
+import com.facebook.genAlgo.mutator.MutatorService;
+import com.facebook.genAlgo.mutator.SingleMutator;
+import com.facebook.genAlgo.utils.RandomProvider;
+import com.facebook.genAlgo.utils.RandomProviderImpl;
+import org.junit.jupiter.api.Test;
+
 public class MainIntegrationTest {
 
+    @Test
+    public void doSomethingTest() {
+        // given
+        char target = 'Z';
+        float mutationChance = 0.01f;
+        int genePoolSize = 1000;
+        RandomProvider randomProvider = new RandomProviderImpl();
+        Evaluator evaluator = new LogarithmicEvaluatorImpl(target);
+        MutatorService mutator = new SingleMutator(randomProvider, mutationChance);
+        CrossoverService crossoverService = new BitPairCrossoverServiceImpl();
+        CrossoverHandler crossoverHandler = new CrossoverHandler(crossoverService);
 
+        GenePoolService genePoolService = new GenePoolService(randomProvider, mutator, evaluator, crossoverHandler, genePoolSize);
+    }
 }
