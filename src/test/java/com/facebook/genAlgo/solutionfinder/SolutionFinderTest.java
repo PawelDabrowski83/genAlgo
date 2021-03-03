2216865457673c49b9f8f8b01951a08b56e7b439
Kamil <s.kamilx@gmail.com>
2021-03-03 17:23:49 +0100

    Update shouldReturnFalseWhenNotFoundSolution method in SolutionFinderTest

--- src/test/java/com/facebook/genAlgo/solutionfinder/SolutionFinderTest.java
+++ src/test/java/com/facebook/genAlgo/solutionfinder/SolutionFinderTest.java
@@ -16,11 +16,6 @@ class SolutionFinderTest {
     List<Gene> geneList = List.of(
             new Gene(randomProvider),
             new Gene(randomProvider),
-            new Gene(randomProvider),
-            new Gene(randomProvider),
-            new Gene(randomProvider),
-            new Gene(randomProvider),
-            new Gene(randomProvider),
             new Gene(randomProvider)
     );
 
@@ -42,13 +37,19 @@ class SolutionFinderTest {
     @Test
     public void shouldReturnFalseWhenNotFoundSolution() {
         // given
+        char target = 'A';
+        char notTarget = 'C';
+        geneList.get(0).setFitness(notTarget);
+        geneList.get(1).setFitness(notTarget);
+        geneList.get(2).setFitness(notTarget);
+
         SolutionFinder solutionFinder = new SolutionFinder(target);
 
         // when
         Boolean result = solutionFinder.findSolution(geneList);
 
         // then
-        assertTrue(result);
+        assertFalse(result);
     }
 
 
