8a86f0874ee34ba8cd6a0afb2e70b8fe6ff00aba
Kamil <s.kamilx@gmail.com>
2021-03-06 23:25:00 +0100

    Add method to test initialization of genes when GenePool is created

--- src/test/java/com/facebook/genAlgo/genepool/GenePoolTest.java
+++ src/test/java/com/facebook/genAlgo/genepool/GenePoolTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -11,12 +12,24 @@ class GenePoolTest {
 
     GenePoolService genePoolService = mock(GenePoolService.class);
 
+
+    @DisplayName("Should initialize List of genes when GenePool is created")
+    @ParameterizedTest
+    @ValueSource(ints = {2, 10, 30, 55, 1000})
+    public void shouldInitializeListOfGenesWhenGenePoolIsCreated(int size) {
+        // given
+        GenePool genePool = new GenePool(genePoolService, size);
+
+        // then
+        assertThat(genePool.getPoolOfGenes().size()).isEqualTo(size);
+    }
+
     @DisplayName("Should increase generation counter when performEvolution() is called")
     @ParameterizedTest
     @ValueSource(ints = {2, 10, 30, 55, 1000})
     public void shouldIncreaseGenerationWhenPerformEvolution(int generation) {
         // given
-        GenePool genePool = new GenePool(genePoolService);
+        GenePool genePool = new GenePool(genePoolService, 10);
         genePool.setGeneration(generation);
 
         // when
