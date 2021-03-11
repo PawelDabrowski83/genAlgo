a9b449857e19306f912dc481f7cf4a317f48d290
Kamil <s.kamilx@gmail.com>
2021-03-11 13:00:43 +0100

    Merge changes from branch 177-GenePoolSolveImpl

--- src/main/java/com/facebook/genAlgo/genepool/GenePool.java
+++ src/main/java/com/facebook/genAlgo/genepool/GenePool.java
@@ -2,11 +2,12 @@ package com.facebook.genAlgo.genepool;
 
 import com.facebook.genAlgo.gene.Gene;
 
+import java.util.Collections;
 import java.util.List;
 
 public class GenePool {
 
-    private GenePoolService genePoolService;
+    private final GenePoolService genePoolService;
     private int generation;
     private final List<Gene> poolOfGenes;
 
@@ -30,7 +31,10 @@ public class GenePool {
     }
 
     public int solve() {
-        return 0;
+        while(!genePoolService.verifySolution(getPoolOfGenes())) {
+            performEvolution();
+        }
+        return generation;
     }
 
     public List<Gene> getPoolOfGenes() {
