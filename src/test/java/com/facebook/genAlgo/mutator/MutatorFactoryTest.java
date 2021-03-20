426efa38020f14abc0e1b1db4d7f65c1c1e91098
Kamil <s.kamilx@gmail.com>
2021-03-20 17:59:12 +0100

    Add method to test getMutator method W/O parameters

--- src/test/java/com/facebook/genAlgo/mutator/MutatorFactoryTest.java
+++ src/test/java/com/facebook/genAlgo/mutator/MutatorFactoryTest.java
@@ -1,8 +1,11 @@
 package com.facebook.genAlgo.mutator;
 
+import com.facebook.genAlgo.mutator.MutatorFactory.MutatorEnum;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class MutatorFactoryTest {
 
@@ -16,7 +19,20 @@ public class MutatorFactoryTest {
         MutatorService mutator = mutatorFactory.getMutator();
 
         // then
-        Assertions.assertThat(mutator).isInstanceOf(SingleMutator.class);
+        assertThat(mutator).isInstanceOf(SingleMutator.class);
+    }
+
+    @DisplayName("Should return SingleMutator with mutationChance equal 0.05 when getMutator() method without parameter is called")
+    @Test
+    public void shouldReturnSingleMutatorWithDefaultMutationChanceWhenGetMutatorIsEvoked() {
+        // given
+        MutatorFactory mutatorFactory = new MutatorFactory();
+
+        // when
+        MutatorService mutator = mutatorFactory.getMutator();
+
+        // then
+        assertThat(((SingleMutator)mutator).getMutationChance()).isEqualTo(0.05f);
     }
 
 
