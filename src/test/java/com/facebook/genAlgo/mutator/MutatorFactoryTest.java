636a7127e8557221c09d2a38661cef3cd2164571
Kamil <s.kamilx@gmail.com>
2021-03-20 14:09:24 +0100

    Add shouldReturnSingleMutatorWhenGetMutatorIsEvoked method to MutatorFactoryTest

--- src/test/java/com/facebook/genAlgo/mutator/MutatorFactoryTest.java
+++ src/test/java/com/facebook/genAlgo/mutator/MutatorFactoryTest.java
@@ -1,4 +1,23 @@
 package com.facebook.genAlgo.mutator;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MutatorFactoryTest {
+
+    @DisplayName("Should return SingleMutator when getMutator() method without parameters is called")
+    @Test
+    public void shouldReturnSingleMutatorWhenGetMutatorIsEvoked() {
+        // given
+        MutatorFactory mutatorFactory = new MutatorFactory();
+
+        // when
+        MutatorService mutator = mutatorFactory.getMutator();
+
+        // then
+        Assertions.assertThat(mutator).isInstanceOf(SingleMutator.class);
+    }
+
+
 }
