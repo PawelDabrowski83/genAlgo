c342c6f480943e9298c88ad036c6ea5b2db081e7
Kamil <s.kamilx@gmail.com>
2021-03-20 14:03:47 +0100

    Create empty class MutatorFactory

---
+++ src/main/java/com/facebook/genAlgo/mutator/MutatorFactory.java
@@ -0,0 +1,11 @@
+package com.facebook.genAlgo.mutator;
+
+public class MutatorFactory {
+
+    MutatorService getMutator(){
+        return null;
+    }
+    MutatorService getMutator(float mutationChance, MutatorEnum option) {
+        return null;
+    }
+}
