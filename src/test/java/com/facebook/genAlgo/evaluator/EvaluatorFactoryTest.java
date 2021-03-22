d14974ba0cad50678c3a9ba42ef99316f21f6438
Paweł Dąbrowski <fluorix@gmail.com>
2021-03-22 21:10:14 +0100

    Create EvaluatorFactoryTest with sample target value.

---
+++ src/test/java/com/facebook/genAlgo/evaluator/EvaluatorFactoryTest.java
@@ -0,0 +1,15 @@
+package com.facebook.genAlgo.evaluator;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class EvaluatorFactoryTest {
+
+    char target;
+
+    @BeforeEach
+    public void init() {
+        target = 'Z';
+    }
+
+
+}
