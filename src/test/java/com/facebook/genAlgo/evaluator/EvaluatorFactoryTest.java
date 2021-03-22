bd4edd91a9376dce409f1e99ff06da83916ef49f
Paweł Dąbrowski <fluorix@gmail.com>
2021-03-22 21:13:23 +0100

    Add EvaluatorFactory as a class field to reuse in each test method.

--- src/test/java/com/facebook/genAlgo/evaluator/EvaluatorFactoryTest.java
+++ src/test/java/com/facebook/genAlgo/evaluator/EvaluatorFactoryTest.java
@@ -1,14 +1,17 @@
 package com.facebook.genAlgo.evaluator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class EvaluatorFactoryTest {
 
     char target;
+    EvaluatorFactory evaluatorFactory;
 
     @BeforeEach
     public void init() {
         target = 'Z';
+        evaluatorFactory = new EvaluatorFactory();
     }
 
 
