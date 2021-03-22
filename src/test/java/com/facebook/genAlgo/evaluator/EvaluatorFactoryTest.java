be8a1f1bebf8807702126d31293e21644e89eff6
Paweł Dąbrowski <fluorix@gmail.com>
2021-03-22 21:23:07 +0100

    Add simple tests for all options of EvaluatorEnum and no arg evokation.

--- src/test/java/com/facebook/genAlgo/evaluator/EvaluatorFactoryTest.java
+++ src/test/java/com/facebook/genAlgo/evaluator/EvaluatorFactoryTest.java
@@ -3,6 +3,8 @@ package com.facebook.genAlgo.evaluator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class EvaluatorFactoryTest {
 
     char target;
@@ -12,6 +14,43 @@ public class EvaluatorFactoryTest {
     public void init() {
         target = 'Z';
         evaluatorFactory = new EvaluatorFactory();
+    }
+
+    @Test
+    public void shouldReturnMaxDeltaEvaluatorWhenGetEvaluatorWithoutArgsIsEvoked() {
+        // when
+        Evaluator evaluator = evaluatorFactory.getEvaluator(target);
+
+        // then
+        assertThat(evaluator).isInstanceOf(MaxDeltaEvaluatorImpl.class);
+    }
+
+    @Test
+    public void shouldReturnMaxDeltaEvaluatorWhenGetEvaluatorWithDefaultArgIsEvoked() {
+        // when
+        Evaluator evaluator = evaluatorFactory.getEvaluator(target, EvaluatorFactory.EvaluatorEnum.DEFAULT);
+
+        // then
+        assertThat(evaluator).isInstanceOf(MaxDeltaEvaluatorImpl.class);
+    }
+
+
+    @Test
+    public void shouldReturnMaxDeltaEvaluatorWhenGetEvaluatorWithMaxDeltaArgIsEvoked() {
+        // when
+        Evaluator evaluator = evaluatorFactory.getEvaluator(target, EvaluatorFactory.EvaluatorEnum.MAX_DELTA);
+
+        // then
+        assertThat(evaluator).isInstanceOf(MaxDeltaEvaluatorImpl.class);
+    }
+
+    @Test
+    public void shouldReturnLogarithmicEvaluatorWhenGetEvaluatorWithLogArgIsEvoked() {
+        // when
+        Evaluator evaluator = evaluatorFactory.getEvaluator(target, EvaluatorFactory.EvaluatorEnum.LOG);
+
+        // then
+        assertThat(evaluator).isInstanceOf(LogarithmicEvaluatorImpl.class);
     }
 
 
