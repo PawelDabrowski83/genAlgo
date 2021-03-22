407a72e51c86801498c140f7a86b1457b65a8e54
Kamil <s.kamilx@gmail.com>
2021-03-22 15:51:36 +0100

    Add implementation of getMutator method with one parameter

--- src/main/java/com/facebook/genAlgo/mutator/MutatorFactory.java
+++ src/main/java/com/facebook/genAlgo/mutator/MutatorFactory.java
@@ -17,7 +17,20 @@ public class MutatorFactory {
         return new SingleMutator(new RandomProviderImpl(), DEFAULT_MUTATION_CHANCE);
     }
     public MutatorService getMutator(MutatorEnum option) {
-
+        switch (option) {
+            case ZERO -> {
+                return new SingleMutator(new RandomProviderImpl(), 0f);
+            }
+            case SINGLE, DEFAULT -> {
+                return new SingleMutator(new RandomProviderImpl(), DEFAULT_MUTATION_CHANCE);
+            }
+            case MULTIPLE -> {
+                return new MultipleMutator(new RandomProviderImpl(), DEFAULT_MUTATION_CHANCE);
+            }
+            default -> {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     public MutatorService getMutator(float mutationChance, MutatorEnum option) {
