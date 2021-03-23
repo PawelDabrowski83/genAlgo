0ae9bdc3f1b4760baed0bb0414181318610bcf70
Kamil <s.kamilx@gmail.com>
2021-03-23 13:19:35 +0100

    Create empty class CrossoverServiceFactory

---
+++ src/main/java/com/facebook/genAlgo/crossover/CrossoverServiceFactory.java
@@ -0,0 +1,12 @@
+package com.facebook.genAlgo.crossover;
+
+public class CrossoverServiceFactory {
+
+    public CrossoverService getCrossoverService() {
+        return null;
+    }
+
+    public CrossoverService getCrossoverService(CrossoverServiceEnum option) {
+        return null;
+    }
+}
