819e89afcd3fadfa69edd9a689b5a8d4ce5c9fa4
Paweł Dąbrowski <fluorix@gmail.com>
2021-04-01 08:44:36 +0200

    Adjust MutatorFactoryTest class.

--- src/test/java/com/facebook/genAlgo/mutator/MutatorFactoryTest.java
+++ src/test/java/com/facebook/genAlgo/mutator/MutatorFactoryTest.java
@@ -1,14 +1,12 @@
 package com.facebook.genAlgo.mutator;
 
-import com.facebook.genAlgo.mutator.MutatorFactory.MutatorEnum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static com.facebook.genAlgo.mutator.MutatorConfig.*;
 
 public class MutatorFactoryTest {
-
-    public static final float DEFAULT_MUTATION_CHANCE = 0.05f;
 
     @DisplayName("Should return SingleMutator when getMutator() method without parameters is called")
     @Test
